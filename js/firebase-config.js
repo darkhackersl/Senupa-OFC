342c70fe274e94fcf01a39c169843337a69c1afd
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-11-03 17:32:04 +0530

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAXTwuy5ACKCRCO4M18_v8f_Bwie2gRIeU",
+  authDomain: "mr-gadgets.firebaseapp.com",
+  projectId: "mr-gadgets",
+  storageBucket: "mr-gadgets.firebasestorage.app",
+  messagingSenderId: "144245989215",
+  appId: "1:144245989215:web:71e02602c87bf0087aa0ea",
+  measurementId: "G-R6H4WRR6V4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
